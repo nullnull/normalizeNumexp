5047147b4cf2ccd591e326d60bd6cdce965e77a3
nullnull <face.the.music721@gmail.com>
2012-12-04 20:49:45 +0900

    almost complete swig-python"

--- swig/python/test_nee.py
+++ swig/python/test_nee.py
@@ -3,8 +3,8 @@
 from numerical_expression_extractor import *
 
 n = NumericalExpressionExtractor("ja")
-text = "私は三人の回になりたい"
+text = "私は三人の回になりたい五回なりたい"
 result = StringVector(0)
-n.extract_numerical_expression(text, "numerical", result)
+n.extract_numerical_expression(text, result)
 for r in result :
     print r

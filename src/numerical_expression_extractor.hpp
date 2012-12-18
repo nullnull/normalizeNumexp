fc6f38e46a6ac7b7fe917b3b5f37d05894845122
nullnull <face.the.music721@gmail.com>
2012-12-18 22:53:08 +0900

    inappropriate expression remover

--- src/numerical_expression_extractor.hpp
+++ src/numerical_expression_extractor.hpp
@@ -1,17 +1,6 @@
-#include "numerical_expression_normalizer.hpp"
-#include "abstime_expression_normalizer.hpp"
-#include "reltime_expression_normalizer.hpp"
-#include "duration_expression_normalizer.hpp"
+#include "inappropriate_expression_remover.hpp"
 
 namespace numerical_expression_extractor{
-
-	struct InappropriateStrings {
-		template <class Archive>
-		void serialize(Archive &ar){
-			ar & MEMBER(str);
-		}
-		std::string str;
-	};
 
 	class NumericalExpressionExtractor{
 	public:
@@ -24,32 +13,12 @@ namespace numerical_expression_extractor{
 											 std::vector<abstime_expression_normalizer::AbstimeExpression>& abstimeexps,
 											 std::vector<reltime_expression_normalizer::ReltimeExpression>& reltimeexps,
 											 std::vector<duration_expression_normalizer::DurationExpression>& durationexps);
-											 
-		void init_inappropriate_stringss(const std::string& language);
-		void init_url_strings();
-		void delete_inappropriate_extraction(const std::string& text,
-																				 std::vector<numerical_expression_normalizer::NumericalExpression>& numexps,
-																				 std::vector<abstime_expression_normalizer::AbstimeExpression>& abstimeexps,
-																				 std::vector<reltime_expression_normalizer::ReltimeExpression>& reltimeexps,
-																				 std::vector<duration_expression_normalizer::DurationExpression>& durationexps);
-		void delete_inappropriate_extraction_using_dictionary(const std::string& text,
-																													std::vector<numerical_expression_normalizer::NumericalExpression>& numexps,
-																													std::vector<abstime_expression_normalizer::AbstimeExpression>& abstimeexps,
-																													std::vector<reltime_expression_normalizer::ReltimeExpression>& reltimeexps,
-																													std::vector<duration_expression_normalizer::DurationExpression>& durationexps);
-		template <class AnyTypeExpression>
-		void delete_inappropriate_extraction_using_dictionary_one_type(std::vector<AnyTypeExpression>& any_type_expressions);
-		template <class AnyTypeExpression>
-		void delete_url_strings(const std::string& text, std::vector<AnyTypeExpression>& any_type_expressions);
-		template <class AnyTypeExpression>
-		bool is_url_strings(const std::string& text, const AnyTypeExpression& any_type_expression);
 		
 		numerical_expression_normalizer::NumericalExpressionNormalizer NEN;
 		abstime_expression_normalizer::AbstimeExpressionNormalizer AEN;
 		reltime_expression_normalizer::ReltimeExpressionNormalizer REN;
 		duration_expression_normalizer::DurationExpressionNormalizer DEN;
-		std::map<std::string, bool> inappropriate_strings_to_bool;
-		std::map<char, bool> url_strings_to_bool;
+		inappropriate_expression_remover::InappropriateExpressionRemover IER;
 	};
 } //namespace numerical_expression_extractor
 
